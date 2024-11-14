10985b17f0287f35d1fa93aefcd63405dd230986
M4rcoos <contato.shopvmax@gmail.com>
2024-11-14 16:06:41 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1 align="center">
+  💻<br>Portfólio - Marcos Vinicius
+</h1>
+
+
+<h4 align="center"><a href="#">Clique para visitar o projeto</a></h4>
+
---
+++ assets/js/main.js
@@ -0,0 +1,103 @@
+import { initScrollReveal } from "./scrollReveal.js";
+import { hoverChangeExperience } from "./hoverChangeExperience.js";
+import { typeWrite } from "./typeWrite.js";
+import { hoverChangeDescription } from "./hoverChangeDescription.js";
+import { menu } from "./menu.js";
+
+menu();
+initScrollReveal();
+typeWrite(document.querySelector(".typewriter"));
+
+hoverChangeExperience(
+  ".Primitecnologia",
+  `Atuo no desenvolvimento de aplicações web e mobile, garantindo um software
+  seguro e entregas no prazo, sou responsavél por contribuir e manter a aplicação mobile
+  com flutter com foco em combater a pirataria "SELO INTELIGENTE".Também atuo no desenvolvimento E2E
+  da aplicação web que tem como objetivo usar a base de dados e mostrar no mapa localização de consultas com status
+  falsos,verdadeiro e suspeito.\n
+  Tecnologias: Flutter | React js | Nest js | Tailwind CSS | Firebase | Google cloud | MYSQL | AWS | Play store | Apple Store
+  `,
+  "Desenvolvedor Fulltsack",
+  "Primi Tecnologia",
+  "Abr 2024 - Atual"
+);
+
+hoverChangeExperience(
+  ".newtesc",
+  `Trabalhei como Desenvolvedor Asp.Net e React Native. Meu foco era construir desde o inicio uma página web a qual 
+   indicava informações em tempo real de radares semafóricos e também programava eles por linhas de comando usando boas práticas de programação com Asp.Net.
+   Com React Native fui responsavél por iniciar uma aplicação que tinha como objetivo programar radares semafóricos. \n
+   Tecnologias: Asp.net | SQL Server | Comunicação via Socket.io, TCP, FTP, Http | Bootstrap | React Native | APK. `,
+  "Software engineer C# | React Native",
+  "New Tesc",
+  "Set 2023 - Abril 2024 (8 meses)"
+);
+
+hoverChangeExperience(
+  ".timeware",
+  `Trabalhei como estágiario por contrato e após 4 meses fui tercerizado para Semantix.\n 
+  Na timeware tambem pude contribuir no desenvolvimento do App mobile com Usando React native com foco em criar um marketing place para 
+  lojistas e consumidores da região do Brás-Sp o maior comércio de varejista e atacado do Brasil.` ,
+  "Developer Mobile",
+  "Timeware",
+  "Mar 2022 - Ago 2023 (1 ano e 5 meses)"
+);
+
+hoverChangeExperience(
+  ".semantix",
+  `Na Semantix atuei como analista backend e as principais responsabilidades era as seguintes. \n
+  Realizar o tracking de tarefas e documentação das entregas. \n
+  Realizar reuniões com os clientes (interno e externo) para entendimento de cenário, mapeamento das demandas, homologações e etc.\n
+  analisar logs de erro e corrigir chamados aberto pelo Jira.`,
+  "Analista Backend",
+  "Semantix",
+  "Jun 2022 - Jan 2023 (8 meses)"
+);
+
+hoverChangeDescription(
+  ".html",
+  "HTML é uma linguagem de marcação, onde marcamos os elementos para definir quais informações a página vai exibir."
+);
+hoverChangeDescription(
+  ".css",
+  "CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas."
+);
+hoverChangeDescription(
+  ".js",
+  "JavaScript é uma linguagem de programação que permite a você implementar elementos dinâmicos em páginas web."
+);
+hoverChangeDescription(
+  ".sass",
+  "Sass é um pré-processador CSS que adiciona alguns recursos que não estão disponíveis de forma nativa."
+);
+hoverChangeDescription(
+  ".react",
+  "React é uma biblioteca JavaScript com foco em criar interfaces de usuário de forma componetizada."
+);
+hoverChangeDescription(
+  ".nest",
+  "O Nest.js é um dos frameworks Node.js de crescimento mais rápido para construir aplicativos backend eficientes, escaláveis e de nível empresarial usando o Node.js.");
+hoverChangeDescription(
+  ".styled",
+  "styled-components é uma biblioteca que utiliza o conceito de CSS-in-JS, ou seja, que nos permite escrever códigos CSS dentro do Javascript."
+);
+hoverChangeDescription(
+  ".tailwind",
+  "Tailwind CSS é um framework CSS que nos fornece classes utilitárias com o propósito de estilizar as páginas."
+);
+hoverChangeDescription(
+  ".typescript",
+  "TypeScript é um superset do JavaScript que incluir recursos que não estão presentes na linguagem de forma nativa, além de torná-la estática."
+);
+hoverChangeDescription(
+  ".mysql",
+  "My SQL é um banco de dados relacional que nos possibilita criar aplicações de grande porte prezando segurança e fornece um ambiente completo para armazenar, gerenciar e recuperar dados em bancos de dados"
+);
+hoverChangeDescription(
+  ".cypress",
+  "Cypress é um framework para automação de testes end-to-end, onde atualmente usa a linguagem JavaScript."
+);
+hoverChangeDescription(
+  ".storybook",
+  "Storybook é uma ferramenta que tem como objetivo documentar e testar os componentes da aplicação."
+);
